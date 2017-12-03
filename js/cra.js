3117ca9846498c3dcf7fc8a60cadc59223ccd51b
jbcade <jesse.cade@gmail.com>
2017-12-03 11:25:53 -0500

    Update cra.js

--- js/cra.js
+++ js/cra.js
@@ -25,7 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
 			statewideCounties[i].classList.remove('no-display');
 			if (i === 0) {
 				statewideCounties[i].selected = true;	
-				event.target.onchange();
+				var event = new Event('change');
+				document.getElementById('county').dispatchEvent(event);
 			}
 			statewideCounties[i].remove();
 			parentSelect.prepend(statewideCounties[i]);
