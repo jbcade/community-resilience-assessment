c09cbf68ce5797023de3aa6035f1c841bcf477eb
jbcade <jesse.cade@gmail.com>
2017-12-14 18:39:19 -0500

    Added Ecomap

--- js/cra.js
+++ js/cra.js
@@ -274,6 +274,38 @@ document.addEventListener("DOMContentLoaded", function() {
 			event.stopPropagation();
 		});
 	}
+	
+    var nodes = new vis.DataSet([
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'}
+    ]);
+
+    // create an array with edges
+    var edges = new vis.DataSet([
+        {from: 1, to: 3},
+        {from: 1, to: 2},
+        {from: 2, to: 4},
+        {from: 2, to: 5}
+    ]);
+
+    // create a network
+    var ecomapContainer = document.getElementById('ecomap');
+
+    // provide the data in the vis format
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+    var options = {
+		height: '100%',
+		width: '100%'
+	};
+
+    // initialize your network!
+    var ecomap = new vis.Network(ecomapContainer, data, options);
 });
 
 function populateDataFramework(dataFramework) {
