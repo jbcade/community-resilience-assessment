859e46c338666b93eadff5e731b726d784dbb001
jbcade <jesse.cade@gmail.com>
2017-12-16 18:49:18 -0500

    Update cra.js

--- js/cra.js
+++ js/cra.js
@@ -257,7 +257,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	$('#activate-map-tab').on('shown.bs.tab', function (e) {
 		google.maps.event.trigger(map, 'resize');
-		map.get_center()
+		console.log(map.getCenter());
 		console.log('Map resized');
 	})
 	
@@ -318,7 +318,7 @@ function initMap(state,county,place) {
 			console.log(JSON.stringify(bounds));
 			console.log(JSON.stringify(results[0].geometry.location));
 			map.fitBounds(bounds);
-			map.get_center()
+			console.log(map.getCenter());
 		} else {
 			console.log('Geocode was not successful for the following reason: ' + status);
 			map = new google.maps.Map(document.getElementById('map'), {
