c892833ccce25743fa2fa876ddd430249771853e
jbcade <jesse.cade@gmail.com>
2017-12-17 13:05:46 -0500

    Update cra.js

--- js/cra.js
+++ js/cra.js
@@ -482,7 +482,7 @@ var cloneRow = function(event) {
 		contentEditables[i].innerHTML = '';
 	}
 	newPlusIcon = clonedNode.querySelector('.add-row').addEventListener('click', cloneRow);
-	newMinusIcon = clonedNode.querySelector('.add-row').addEventListener('click', deleteRow);
+	newMinusIcon = clonedNode.querySelector('.remove-row').addEventListener('click', deleteRow);
 	referenceNode.parentNode.insertBefore(clonedNode, referenceNode.nextSibling);
 }
 
