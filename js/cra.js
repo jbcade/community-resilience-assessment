55dd77860021371ee329940bdf8d44b2fa33a70f
jbcade <jesse.cade@gmail.com>
2017-12-24 00:06:30 -0500

    Update cra.js

--- js/cra.js
+++ js/cra.js
@@ -380,7 +380,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		edgeEvent.event.preventDefault();
         	console.log('selectEdge Event:', edgeEvent);
 		var targetEdgeID = edgeEvent.edges[0];
-		console.log(edges.get(targetEdgeID,{fields: ['id']});
+		console.log(edges.get(targetEdgeID,{fields: ['id']}));
 		edges.update([{id:targetEdgeID, color:{color:"red"}, state: 1}]);
     	});
 	network.fit();
